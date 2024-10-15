d44e77a2fa45945d91079dd10b4b75a3893fd616
Vinicius Basso <129125909+ViniciusBassow@users.noreply.github.com>
2024-10-14 22:32:08 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ehr_csharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
