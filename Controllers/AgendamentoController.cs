243331ea457ffc34f71463d964e10098767c49a2
Vinicius Basso <129125909+ViniciusBassow@users.noreply.github.com>
2024-11-01 23:14:35 -0300

    30/10/2024 Vinicius Felipe 23:14

--- Controllers/AgendamentoController.cs
+++ Controllers/AgendamentoController.cs
@@ -1,6 +1,8 @@
 ﻿using ehr_csharp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using SQLApp.Data;
 using System.Diagnostics;
 
@@ -8,18 +10,45 @@ namespace ehr_csharp.Controllers
 {
     public class ConsultaController : GlobalController
     {
-        public ConsultaController(AppDbContext context) : base(context)
+        private readonly IMemoryCache _cache;
+        public ConsultaController(AppDbContext context, IMemoryCache cache) : base(context)
         {
+            _cache = cache;
         }
 
         //wwwroot\Calendar\fullcalendar\packages\core\main.js           -> CONTROLE CALENDARIUO
         //wwwroot\Calendar\fullcalendar\packages\core\main.css          -> ESTILOS
         public ActionResult Index()
         {
-            List<Consulta> consultas = Contexto<Consulta>().ToList();
-            
+            List<Consulta> consultas = new List<Consulta>();
+            if (_cache.TryGetValue("UsuarioLogado", out Usuario UsuarioLogado))
+            {
+                if (UsuarioLogado.Medico != null)
+                    consultas.AddRange(Contexto<Consulta>().Include(x => x.Paciente).Where(x => x.IdMedico == UsuarioLogado.Medico.Id).ToList());
+                else if (UsuarioLogado.Role == "Admin")
+                    consultas.AddRange(Contexto<Consulta>().Include(x => x.Paciente).ToList());
+            }
 
             return View("Views\\Consulta\\Index.cshtml", consultas);
+        }
+
+
+
+
+        [HttpPost]
+        public async Task<ActionResult> Salvar(Consulta consulta)
+        {
+
+
+            if (!ModelState.IsValid)
+            {
+                return View("Views\\Usuario\\editar.cshtml", new Consulta());
+            }
+
+            Contexto<Consulta>().Add(consulta);
+
+            SaveChanges();
+            return View("Views\\Consulta\\Index.cshtml", consulta);
         }
     }
 }
