a663a764aec2d53aabc6dcf0fa0ac8f55f654bee
Vinicius Basso <129125909+ViniciusBassow@users.noreply.github.com>
2024-11-03 21:46:17 -0300

    Reapply "Merge branch 'master' of https://github.com/ViniciusBassow/ehr-csharp"
    This reverts commit 42760d8a5322fb5350c001729026419d5babb0cf.

--- Migrations/20241103013344_AddStatusConsulta.Designer.cs
+++ Migrations/20241103013344_AddStatusConsulta.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SQLApp.Data;
 
@@ -11,9 +12,11 @@ using SQLApp.Data;
 namespace ehr_csharp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241103013344_AddStatusConsulta")]
+    partial class AddStatusConsulta
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -201,10 +204,6 @@ namespace ehr_csharp.Migrations
                         .HasColumnType("int");
 
                     b.Property<string>("Motivo")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("MotivoCancelamento")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
