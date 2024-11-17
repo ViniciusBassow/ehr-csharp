dab6c4b53f7b6d1f280c83cd9f7542e6d25b0458
Pedro Abel <pedrooabell@gmail.com>
2024-11-16 21:35:42 -0300

    16/11/2024 Pedro Abel 21:35

--- Controllers/AdministratorController.cs
+++ Controllers/AdministratorController.cs
@@ -1,6 +1,46 @@
-﻿namespace ehr_csharp.Controllers
+﻿using ehr_csharp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using SQLApp.Data;
+
+namespace ehr_csharp.Controllers
 {
-    public class AdministratorController
+    public class AdministratorController : GlobalController
     {
+
+        public AdministratorController(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Usuarios()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Calendario()
+        {
+            return View();
+        }
+
+
+        public async Task<ActionResult> Relatorios()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Configuracoes()
+        {
+            return View();
+        }
+
     }
 }
